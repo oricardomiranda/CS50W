9b6a24529d44e9e8643535d6dea4a88c9b83ac1c
oricardomiranda <88597113+oricardomiranda@users.noreply.github.com>
2024-03-05 01:34:03 +0000

    Support for other browsers

--- Final Project/Capstone/project5/tests/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class Hash(models.Model):
-    text = models.TextField()
-    hash = models.CharField(max_length = 64)
--- Lecture 2 - Python/name.py
+++
@@ -1,3 +0,0 @@
-name = input("What's your name? ")
-
-print(f"Nice to meet you {name}")
